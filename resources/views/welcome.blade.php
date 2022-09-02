bbf680584ace7891909f444902860a624b525f8e
Jeffrey Woods <jeffreytwoods2@icloud.com>
2022-09-01 23:59:12 -0600

    Added apple touch icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <meta property="og:site_name" content="jarumi.soy" />
         <meta property="og:image" content="{{ asset('img/hermosa.jpeg') }}" />
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
 
         <title>Jarumi Woods - Spanish DLI</title>
 
